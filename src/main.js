311411c7d51f3de04b03b1940c5a21a58afb4f42
Victor Marcano <152332746+lVictor80kl@users.noreply.github.com>
2025-03-25 01:51:28 -0400

    ...

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css' // Add this if you have global styles
 
 createApp(App).mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
   server: {
-    port: 3000,
-  },
+    port: 3000
+  }
 })
