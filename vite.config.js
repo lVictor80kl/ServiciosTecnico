0c4704d3733d91d889bd1b90f3b78c12aad46177
Victor Marcano <152332746+lVictor80kl@users.noreply.github.com>
2025-03-25 01:41:24 -0400

    Probando porque no inicializa

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-// filepath: vite.config.js
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import { resolve } from 'path';
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'), // Alias para rutas
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
-    port: 3000, // Cambia el puerto aquí
+    port: 3000,
   },
-});
+})
